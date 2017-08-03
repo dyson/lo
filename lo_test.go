fd5df59eddf2318d32c5b4e17457e423ffde2a80
Dyson Simmons <dysonsimmons@gmail.com>
2017-08-03 13:31:11 +0100

    Fix tests after adding license header

--- lo_test.go
+++ lo_test.go
@@ -24,7 +24,7 @@ const (
 	Rdate         = `[0-9][0-9][0-9][0-9]/[0-9][0-9]/[0-9][0-9]`
 	Rtime         = `[0-9][0-9]:[0-9][0-9]:[0-9][0-9]`
 	Rmicroseconds = `\.[0-9][0-9][0-9][0-9][0-9][0-9]`
-	Rline         = `(58|80):` // must update if the calls to l.Printf / l.Print below move
+	Rline         = `(60|82):` // must update if the calls to l.Printf / l.Print below move
 	Rlongfile     = `.*/[A-Za-z0-9_\-]+\.go:` + Rline
 	Rshortfile    = `[A-Za-z0-9_\-]+\.go:` + Rline
 )
